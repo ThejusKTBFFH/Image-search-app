0f8eff5b3a271cc1902370ca0737df0b912b6c17
ThejusKTBFFH <1thejusjoshi@gmail.com>
2023-04-25 18:32:14 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  function handleSearch(event) {
+    event.preventDefault();
+    alert("Searching...");
+  }
+
+  const [searchTerm, setSearchTerm] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>React Photo Search</h1>
+
+      <form>
+        <input type="text" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
+        <button onClick={handleSearch}>Search</button>
+      </form>
+
+
+      
     </div>
   );
 }
